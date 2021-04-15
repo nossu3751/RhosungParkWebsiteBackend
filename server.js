57164bce25dcedd395a4dbba11bc62c9d2796fdc
Rhosung Park <rhosungpark@gmail.com>
2021-04-15 00:26:35 -0700

    access control allow origin added

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(cors({
     origin: ["http://localhost:4200", "http://www.rhosungpark.com"],
     optionsSuccessStatus: 200
 }))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 import EmailRoute from './routes/email';
 import AuthRoute from './routes/auth';
